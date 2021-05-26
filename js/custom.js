8008e378a98b68959850a0495d558bd88af170ae
Sadia-Ashrafi <sadiaashrafi09@gmail.com>
2021-05-26 20:17:56 +0600

    file added

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+
+for (i=1; i<=100; i++){
+    if(i % 2 !== 0){
+    document.write (i +  "<br>");
+    }
+    if(i % 2 == 0){
+        document.write (i + " is an  even number" + "<br>");
+        }
+}
+
+
